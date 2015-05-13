bbaa6d91bbf68df67c7386b900a43c930a120a2a
Brady Africk <foresterapps@gmail.com>
2015-05-13 14:25:56 -0500

    sdf

---  Game Code/backend/main.java
+++  Game Code/backend/main.java
@@ -1,10 +1,5 @@
 
-/**
- * Write a description of class main here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class main
 {
     // instance variables - replace the example below with your own
