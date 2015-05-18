45b143f133d58190da9c98787107e8ecd3bd90ae
Brady Africk <foresterapps@gmail.com>
2015-05-18 14:12:52 -0500

    added two classes; they do nothing

---
+++ Game Code/Game.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Game
+{
+    // instance variables - replace the example below with your own
+    
+    ArrayList<Player> players = new ArrayList<Player>();
+    /**
+     * Constructor for objects of class Game
+     */
+    public Game()
+    {
+
+
+    }
+    
+    public void run(){
+        
+        
+    }
+
+}
--- Game Code/Server.java
+++ Game Code/Server.java
@@ -3,20 +3,24 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
+import java.util.ArrayList;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 public class Server {
 
+    ArrayList<Game> games = new ArrayList<Game>();
+    
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/test", new MyHandler());
+        server.createContext("/games/create", new CreateGame());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
 
-    static class MyHandler implements HttpHandler {
+    static class CreateGame implements HttpHandler {
         public void handle(HttpExchange t) throws IOException {
             String response = "HEY MOTHERFUCKER THIS SHIT WORKS OK?";
             JSONObject WHATEVER = new JSONObject();
