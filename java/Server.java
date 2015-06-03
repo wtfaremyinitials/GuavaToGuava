b6791d096aa4189e083f8438c0eba515e66cbbf2
Brady Africk <bafrick30@gmail.com>
2015-06-03 12:23:22 -0500

    FINISHED COMMENTS

--- java/Server.java
+++ java/Server.java
@@ -44,83 +44,82 @@ public class Server {
             games.add(g);                                                                  //adds it to the arraylist
             String response = games.size()-1 + "";                                         //sets the reponse to the last item in the arraylist
             httpserver.createContext("/games/" + response + "/status", new GetStatus(g));  //creates the status url using the game ID
-            t.sendResponseHeaders(200, response.length());                                 
+            t.sendResponseHeaders(200, response.length());                                 //send the response
             OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+            os.write(response.getBytes());                                                 //writes the reponse
+            os.close();                                                                    //closes the response
         }
     }
 
     //converts the array list of games into a string and returns it to the server
     static class GameArray implements HttpHandler {
         public void handle(HttpExchange t) throws IOException {
-            JSONArray array = new JSONArray();
-            for (int x = 0; x < games.size(); x++){
+            JSONArray array = new JSONArray();                                             //creates a new JSONArray
+            for (int x = 0; x < games.size(); x++){                                        //for loop to put an ID with each game
                 array.put(x);
             }
-            String response = array.toString(); 
-            if (response == null || response.equals("")){ response = "null"; }
-            t.sendResponseHeaders(200, response.length());
+            String response = array.toString();                                            //converts the array to string
+            if (response == null || response.equals("")){ response = "null"; }             //checks if the response equals null
+            t.sendResponseHeaders(200, response.length());                                 //send the response
             OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+            os.write(response.getBytes());                                                 //writes the reponse
+            os.close();                                                                    //closes the response
         }
     }
 
     //converts the array list of games into a string and returns it to the server
     static class GetStatus implements HttpHandler {
         
-        private Game game;
+        private Game game;                                                                         //creates a game
         
         public GetStatus(Game game) {
-            this.game = game;
+            this.game = game;                                                                      //sets the game equals this.game
         }
         
         public void handle(HttpExchange t) throws IOException {
-            HashMap<String, String> hm = queryToMap(t.getRequestURI().getQuery());
-            int playerId = Integer.parseInt(hm.get("pid"));
-            Player player = game.getPlayers().get(playerId);
+            HashMap<String, String> hm = queryToMap(t.getRequestURI().getQuery());                 //creats a dual string HashMap
+            int playerId = Integer.parseInt(hm.get("pid"));                                        //creates an int with an id of "pid"
+            Player player = game.getPlayers().get(playerId);                                       //creates a player named player from the array of players  
+            JSONArray cardsArray = new JSONArray();                                                //creates a JSONArray called cardsArray
             
-            JSONArray cardsArray = new JSONArray(); 
-            
-            if(game.getCzar() != playerId) {
-                for(Card c : player.getCards()) {
-                    cardsArray.put(c.getId());
+            if(game.getCzar() != playerId) {                                                       //If the czar doesnt equal the playerID
+                for(Card c : player.getCards()) {                                                  //for loop that goes through the players cards
+                    cardsArray.put(c.getId());                                                     //puts the card in the array
                 } 
             }    
                     
-            JSONArray playersArray = new JSONArray();
+            JSONArray playersArray = new JSONArray();                                              //creates a JSON playersArray
             
-            for(int i=0; i<game.getPlayers().size(); i++) {
-                JSONObject p = new JSONObject();
-                p.putOpt("name",  game.getPlayers().get(i).getName());
-                p.putOpt("score", null);
-                p.putOpt("czar",  null);
-                playersArray.put(p);
+            for(int i=0; i<game.getPlayers().size(); i++) {                                        //for loop that continues until i is less than the size of players     
+                JSONObject p = new JSONObject();                                                   //creates a new JSONObject
+                p.putOpt("name",  game.getPlayers().get(i).getName());                             //adds name to the name of the player
+                p.putOpt("score", null);                                                           //puts score and null
+                p.putOpt("czar",  null);                                                           //puts czar and null
+                playersArray.put(p);                                                               //puts p in the player's array
             }
             
-            JSONObject jsobj = new JSONObject();
-            jsobj.putOpt("players", playersArray);
-            jsobj.putOpt("cards",   cardsArray);
-            String response = jsobj.toString();
-            t.sendResponseHeaders(200, response.length());
+            JSONObject jsobj = new JSONObject();                                                   //creates a new jsobj
+            jsobj.putOpt("players", playersArray);                                                 //puts players in the playersArray
+            jsobj.putOpt("cards",   cardsArray);                                                   //puts players in the cardsArray
+            String response = jsobj.toString();                                                    //converts the JSON object to string
+            t.sendResponseHeaders(200, response.length());                                         //send the response
             OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+            os.write(response.getBytes());                                                         //writes the reponse
+            os.close(); 
         }
     }
 
-    public static HashMap<String, String> queryToMap(String query){
-        HashMap<String, String> result = new HashMap<String, String>();
-        for (String param : query.split("&")) {
-            String pair[] = param.split("=");
+    public static HashMap<String, String> queryToMap(String query){              
+        HashMap<String, String> result = new HashMap<String, String>();           //creates the Hashmap
+        for (String param : query.split("&")) {                                   //for loop to split the querey 
+            String pair[] = param.split("=");                                     //creates the array with the split param with "="
             if (pair.length>1) {
-                result.put(pair[0], pair[1]);
+                result.put(pair[0], pair[1]);                                     //if the pair is longer than 1, put 0 and 1
             }else{
-                result.put(pair[0], "");
+                result.put(pair[0], "");                                          //if the pair is anything else is longer than 1, put 0 and 1
             }
         }
-        return result;
+        return result;                                                            //returns the result
     }
 
 }
